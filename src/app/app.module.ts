7e25217602dcb5124ffd7d000c1404c3e1393720
Nicxv <nico0valenzuelacl@gmail.com>
2024-09-10 16:04:21 -0300

    vinculacion con firebase
    solo vinculacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,30 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
+//-------Firebase imports-------//
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+//--sistema de autenticacion 
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+//--para que funcione la base de datos
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+
+
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAjeDGC_iyfAVa3Q4v4DQkLsKMPIAi9dW8",
+    authDomain: "saludya-ad59b.firebaseapp.com",
+    projectId: "saludya-ad59b",
+    storageBucket: "saludya-ad59b.appspot.com",
+    messagingSenderId: "578492903787",
+    appId: "1:578492903787:web:1dda569573b7fa4e285c2e",
+    measurementId: "G-5SZVW39CHS"
+    }
 };

0995caaaa076b1f0598d3a773169042869f13676
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2024-10-09 13:23:18 -0300

    lista mockup

--- src/app/pages/lista-doc/lista-doc.page.ts
+++ src/app/pages/lista-doc/lista-doc.page.ts
@@ -8,6 +8,29 @@ import { registroUsuario } from 'src/app/models/models';
   styleUrls: ['./lista-doc.page.scss'],
 })
 export class ListaDocPage implements OnInit {
+
+  especialidades = [
+    {
+      id: 1,
+      name: 'Psiquiatra',
+    },
+    {
+      id: 2,
+      name: 'Psicólogo',
+    },
+    {
+      id: 3,
+      name: 'Gastroenterólogo',
+    },
+    {
+      id: 4,
+      name: 'Otorrino',
+    },
+  ];
+
+
+
+
   UsuariosFiltrados: registroUsuario[] = [];
 
   constructor(private firestoreService: FirestoreService) { }

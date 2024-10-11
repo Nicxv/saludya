de3387926c1b14d45d475cf951e2c737d44f42df
Nicxv <nico0valenzuelacl@gmail.com>
2024-10-11 13:02:42 -0300

    lista-doc terminado

--- src/app/pages/lista-doc/lista-doc.page.ts
+++ src/app/pages/lista-doc/lista-doc.page.ts
@@ -9,15 +9,9 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./lista-doc.page.scss'],
 })
 export class ListaDocPage implements OnInit {
-
-  especialidades = [
-    { id: 1, name: 'Psiquiatra' },
-    { id: 2, name: 'Psicólogo' },
-    { id: 3, name: 'Gastroenterólogo' },
-    { id: 4, name: 'Otorrino' },
-  ];
-
   UsuariosFiltrados: registroUsuario[] = [];
+  filteredUsuarios: registroUsuario[] = []; // Almacenar los usuarios filtrados
+  searchTerm: string = ''; // Almacenar el término de búsqueda
   userRole: string | null = null;
 
   constructor(private firestoreService: FirestoreService, private authService: AuthService) { }
@@ -26,10 +20,16 @@ export class ListaDocPage implements OnInit {
     this.cargarUsuarios();
     this.getUserRole();
   }
+  clearSearch() {
+    this.searchTerm = '';
+    this.filteredUsuarios = this.UsuariosFiltrados; // Mostrar todos los usuarios cuando se borra el término de búsqueda
+  }
+  
 
   cargarUsuarios() {
     this.firestoreService.getUsuarios().subscribe(data => {
       this.UsuariosFiltrados = data.filter(usuario => usuario.rol === 'funcionario');
+      this.filteredUsuarios = this.UsuariosFiltrados; // Inicializar la lista filtrada
       this.UsuariosFiltrados.forEach(usuario => {
         usuario.isEditing = false; // Para controlar la edición
       });
@@ -41,6 +41,16 @@ export class ListaDocPage implements OnInit {
       this.userRole = role;
     });
   }
+
+  filterUsuarios() {
+    const searchLower = this.searchTerm.toLowerCase();
+  
+    this.filteredUsuarios = this.UsuariosFiltrados.filter(usuario => {
+      // Asegurarse de que `usuario.especialidad` esté definido antes de llamar a `toLowerCase`
+      return usuario.especialidad && usuario.especialidad.toLowerCase().includes(searchLower);
+    });
+  }
+  
 
   enableEdit(usuario: registroUsuario) {
     usuario.isEditing = true;
@@ -61,3 +71,4 @@ export class ListaDocPage implements OnInit {
     usuario.isEditing = false; // Cancelar la edición
   }
 }
+
